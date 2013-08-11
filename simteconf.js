1794dba267a30abe4745886b96fc54b218a9c08e
Mithgol the Webmaster <getgit@mithgol.ru>
2013-08-11 13:28:15 +0400

    the first working test-covered version (without API docs)

--- simteconf.js
+++ simteconf.js
@@ -1,7 +1,79 @@
+var fs = require('fs');
+var os = require('os');
+var extend = require('util-extend');
+var _ = require('underscore');
+_.str = require('underscore.string');
+_.mixin(_.str.exports());
+
+var defaults = {
+   EOL: os.EOL,
+   skipEmpty: true,
+   lowercase: true
+}
+
 var simteconf = function(filename, options){
    if(!( this instanceof simteconf )){
       return new simteconf(filename, options);
    }
+   this.options = extend(defaults, options);
+
+   this.lines = {};
+
+   var contents, fileLines;
+   try{
+      contents = fs.readFileSync(filename, {
+         encoding: 'utf8'
+      });
+   }catch(e){
+      return;
+   }
+
+   fileLines = contents.split( this.options.EOL );
+   contents = null;
+
+   _(fileLines).each(function(fileLine){
+      fileLine = fileLine.trim();
+      if( fileLine.length < 1 ) return;
+
+      var name = _(fileLine).strLeft(' ');
+      var content = _(fileLine).strRight(' ');
+      content = content.trimLeft();
+      if( this.options.skipEmpty && content.length < 1 ) return;
+
+      if( this.options.lowercase ) name = name.toLowerCase();
+
+      if( !_(this.lines).has(name) ){
+         this.lines[name] = [];
+      }
+      this.lines[name].push(content);
+   }, this);
+}
+
+simteconf.prototype.first = function(name){
+   if( this.options.lowercase ) name = name.toLowerCase();
+
+   if( !_(this.lines).has(name) ){
+      return null;
+   }
+   return _(this.lines[name]).first();
+}
+
+simteconf.prototype.last = function(name){
+   if( this.options.lowercase ) name = name.toLowerCase();
+
+   if( !_(this.lines).has(name) ){
+      return null;
+   }
+   return _(this.lines[name]).last();
+}
+
+simteconf.prototype.all = function(name){
+   if( this.options.lowercase ) name = name.toLowerCase();
+
+   if( !_(this.lines).has(name) ){
+      return null;
+   }
+   return this.lines[name];
 }
 
 module.exports = simteconf;
--- test/test.js
+++ test/test.js
@@ -3,8 +3,24 @@ var path = require('path');
 var assert = require('assert');
 var filename = path.join(__dirname, 'simteconf.conf');
 
-describe('Test integrity', function(){
-   it('Test file is found', function(){
+describe('Integrity of the test', function(){
+   it('test file is found', function(){
       assert.ok( fs.existsSync(filename) );
+   });
+});
+
+var simteconf = require('..');
+var conf = simteconf(filename);
+
+describe('Simple configuration', function(){
+   it('not repeated lines', function(){
+      assert.equal( conf.first('samia'), 'dostika' );
+      assert.equal( conf.last('ari'), 'adritida' );
+      assert.deepEqual( conf.all('tori'), ['adito madola'] );
+   });
+   it('repeated lines', function(){
+      assert.equal( conf.first('witch'), 'Charlotte' );
+      assert.equal( conf.last('Witch'), 'Kriemhild Gretchen' );
+      assert.deepEqual( conf.all('witch'), ['Charlotte', 'Oktavia', 'Kriemhild Gretchen'] );
    });
 });
