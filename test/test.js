fab6b6c9fb5df452be6d5a494b8093ff82d474aa
Mithgol the Webmaster <getgit@mithgol.ru>
2013-08-22 15:24:46 +0400

    BDD verbs in tests

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ var path = require('path');
 var assert = require('assert');
 var filename = path.join(__dirname, 'simteconf.conf');
 
-describe('Integrity of the test', function(){
-   it('test file is found', function(){
+describe('The test itself', function(){
+   it('contains the test file', function(){
       assert.ok( fs.existsSync(filename) );
    });
 });
@@ -16,28 +16,30 @@ var noComments = simteconf(filename, {
 });
 
 describe('Simple configuration', function(){
-   it('not repeated lines', function(){
+   it('works with not repeated lines', function(){
       assert.equal( conf.first('samia'), 'dostika' );
       assert.equal( conf.last('ari'), 'adritida' );
       assert.deepEqual( conf.all('tori'), ['adito madola'] );
    });
-   it('repeated lines', function(){
+   it('gets one (or more) of repeated lines', function(){
       assert.equal( conf.first('witch'), 'Charlotte' );
       assert.equal( conf.last('Witch'), 'Kriemhild Gretchen' );
       assert.deepEqual( conf.all('witch'), ['Charlotte', 'Oktavia', 'Kriemhild Gretchen'] );
    });
-   it('random selection', function(){
+   it('makes random selections', function(){
       assert.equal( conf.random('samia'), 'dostika' );
       assert.equal( conf.random('tori'), 'adito madola' );
       assert.ok( conf.all('witch').indexOf( conf.random('witch') ) > -1 );
    });
-   it('missing lines', function(){
+   it('returns null for missing lines', function(){
       assert.equal( conf.first('Mami'), null );
       assert.equal( conf.last(' '), null );
    });
-   it('comment processing', function(){
+   it('processes comments', function(){
       assert.equal( conf.first('#'), 'Sis puella magica!' );
       assert.equal( conf.first('//'), 'the previous line contains a whitespace' );
+   });
+   it('ignores comments (when told to ignore)', function(){
       assert.equal( noComments.last('#'), null );
       assert.equal( noComments.first('//'), null );
    });
